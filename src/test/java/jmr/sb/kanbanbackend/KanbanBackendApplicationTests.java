661283e49169d374fbddabe02a8b0a37b3d4c5ff
Jean-Marc Robin <jmrobin2pi@gmail.com>
2019-02-14 18:54:51 +0100

    Initial commit

---
+++ src/test/java/jmr/sb/kanbanbackend/KanbanBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package jmr.sb.kanbanbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KanbanBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
